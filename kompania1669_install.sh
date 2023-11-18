4da861d60b0bd1900ea690bf29c51a5922de0b32
ShaheenHossain <rapidgrps@princegroup-bd.com>
2023-11-19 02:32:41 +0600

    Update kompania1669_install.sh

--- kompania1669_install.sh
+++ kompania1669_install.sh
@@ -102,7 +102,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install kompania
 #--------------------------------------------------
 echo -e "\n==== Installing kompania Server ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/princeforchu_ent16 $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/princeforchu_entm16 $OE_HOME_EXT/
 
 if [ $IS_ENTERPRISE = "True" ]; then
     # kompania Enterprise install!
